61adc311c2f356d7a15139a32352946038c46b23
Fedor Shebanin <fashebanin@edu.hse.ru>
2026-08-12 22:58:39 +0300

    uint64_t

--- everybit/bitarray.c
+++ everybit/bitarray.c
@@ -30,6 +30,7 @@
 #include <assert.h>
 #include <limits.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -198,22 +199,14 @@ void bitarray_rotate(bitarray_t* const bitarray,
                          modulo(-bit_right_amount, bit_length));
 }
 
-typedef unsigned char pocket;
+typedef u_int64_t pocket;
 constexpr unsigned pocket_bit_size = sizeof(pocket) * CHAR_BIT;
-constexpr pocket max_pocket = (1u << pocket_bit_size) - 1;
-constexpr pocket reversed_bytes[1u << pocket_bit_size] = {
-    0, 128, 64, 192, 32, 160, 96, 224, 16, 144, 80, 208, 48, 176, 112, 240, 8, 136, 72, 200, 40, 168, 104, 232, 24, 152,
-    88, 216, 56, 184, 120, 248, 4, 132, 68, 196, 36, 164, 100, 228, 20, 148, 84, 212, 52, 180, 116, 244, 12, 140, 76,
-    204, 44, 172, 108, 236, 28, 156, 92, 220, 60, 188, 124, 252, 2, 130, 66, 194, 34, 162, 98, 226, 18, 146, 82, 210,
-    50, 178, 114, 242, 10, 138, 74, 202, 42, 170, 106, 234, 26, 154, 90, 218, 58, 186, 122, 250, 6, 134, 70, 198, 38,
-    166, 102, 230, 22, 150, 86, 214, 54, 182, 118, 246, 14, 142, 78, 206, 46, 174, 110, 238, 30, 158, 94, 222, 62, 190,
-    126, 254, 1, 129, 65, 193, 33, 161, 97, 225, 17, 145, 81, 209, 49, 177, 113, 241, 9, 137, 73, 201, 41, 169, 105,
-    233, 25, 153, 89, 217, 57, 185, 121, 249, 5, 133, 69, 197, 37, 165, 101, 229, 21, 149, 85, 213, 53, 181, 117, 245,
-    13, 141, 77, 205, 45, 173, 109, 237, 29, 157, 93, 221, 61, 189, 125, 253, 3, 131, 67, 195, 35, 163, 99, 227, 19,
-    147, 83, 211, 51, 179, 115, 243, 11, 139, 75, 203, 43, 171, 107, 235, 27, 155, 91, 219, 59, 187, 123, 251, 7, 135,
-    71, 199, 39, 167, 103, 231, 23, 151, 87, 215, 55, 183, 119, 247, 15, 143, 79, 207, 47, 175, 111, 239, 31, 159, 95,
-    223, 63, 191, 127
-};
+constexpr pocket max_pocket = UINT64_MAX;
+
+pocket reverse_pocket(pocket x)
+{
+    return __builtin_bitreverse64(x);
+}
 
 static size_t pocket_reverse(pocket* arr, const size_t shift, const size_t cnt)
 {
@@ -222,22 +215,22 @@ static size_t pocket_reverse(pocket* arr, const size_t shift, const size_t cnt)
     {
         for (; 2 * iters + 1 < cnt; iters++)
         {
-            const pocket right = reversed_bytes[arr[cnt - iters - 1]];
-            arr[cnt - iters - 1] = reversed_bytes[arr[iters]];
+            const pocket right = reverse_pocket(arr[cnt - iters - 1]);
+            arr[cnt - iters - 1] = reverse_pocket(arr[iters]);
             arr[iters] = right;
         }
         return iters;
     }
     for (; 2 * iters + 1 < cnt; iters++)
     {
-        const pocket left = reversed_bytes[arr[cnt - iters] << (pocket_bit_size - shift) & max_pocket];
-        const pocket right = reversed_bytes[arr[cnt - iters - 1] >> shift];
+        const pocket left = reverse_pocket(arr[cnt - iters] << (pocket_bit_size - shift) & max_pocket);
+        const pocket right = reverse_pocket(arr[cnt - iters - 1] >> shift);
 
         arr[cnt - iters] = arr[cnt - iters] >> shift << shift;
-        arr[cnt - iters] |= reversed_bytes[arr[iters] << (pocket_bit_size - shift) & max_pocket];
+        arr[cnt - iters] |= reverse_pocket(arr[iters] << (pocket_bit_size - shift) & max_pocket);
 
-        arr[cnt - iters - 1] &= (1u << shift) - 1;
-        arr[cnt - iters - 1] |= reversed_bytes[arr[iters] >> shift];
+        arr[cnt - iters - 1] &= (1ull << shift) - 1;
+        arr[cnt - iters - 1] |= reverse_pocket(arr[iters] >> shift);
 
         arr[iters] = left | right;
     }
